db1c0579cd807b866c93b13d826f442d9ae1b8d1
Dephinate <dephinate@pop-os.localdomain>
2024-03-03 20:28:36 -0700

    updated Entities

---
+++ src/misterRetriveRite/entity/__init__.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass(frozen=True)
+class VectorizationConfig:
+    root_dir: Path
+    encoder_name: str
+    model_ckpt: Path
+    data_path: Path
+    k:int
+    num_of_cells: int
+    nprobe: int
+
+@dataclass(frozen=True)
+class ModelConfig:
+    model_name: str
+    temperature: int
+    max_tokens: int
